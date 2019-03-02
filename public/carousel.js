9bc00e1aaa44bdf4e76b9d346b6976762f2202e5
GraceHanssen <saccharin07@hotmail.com>
2019-03-02 20:49:44 +0100

    Create carousel.js

---
+++ public/carousel.js
@@ -0,0 +1,51 @@
+function typeEffect(element, speed) {
+    var text = $(element).text();
+    $(element).html('');
+
+    var i = 0;
+    var timer = setInterval(function() {
+        if (i < text.length) {
+            $(element).append(text.charAt(i));
+            i++;
+        } else {
+            clearInterval(timer);
+        }
+    }, speed);
+}
+
+$( document ).ready(function() {
+    var speed = 75;
+    var delay = $('h5').text().length * speed + speed;
+    typeEffect($('h5'), speed);
+    setTimeout(function(){
+        $('p5').css('display', 'inline-block');
+        typeEffect($('p5'), speed);
+    }, delay);
+});
+$( document ).ready(function() {
+    var speed = 75;
+    var delay = $('h6').text().length * speed + speed;
+    typeEffect($('h6'), speed);
+    setTimeout(function(){
+        $('p6').css('display', 'inline-block');
+        typeEffect($("p6"), speed);
+    }, delay);
+});
+
+
+//carousel slider autoplay start and stop on mousehover and mouseleave
+var owl = $(".active");
+owl.owlCarousel({
+    loop:true,
+    smartSpeed:450,
+    responsiveClass: true,
+    responsiveRefreshRate : 10,
+    items:1,
+});
+
+$('.carousel-indicators li.carousel-indicators').on('mouseover',function(e){
+    owl.trigger('play.owl.autoplay');
+})
+$('.carousel-indicators li.product').on('mouseleave',function(e){
+    owl.trigger('stop.owl.autoplay');
+})
